0bae5bf0c2431cb777766ebbf150ce6a410cd0a4
visualstudio <visualstudio@122-PC27>
2019-03-27 09:55:12 -0400

    second commit

--- Assignment 4 -C0731909/Program.cs
+++ Assignment 4 -C0731909/Program.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Assignment_4__C0731909
 {
+
+    // Student id : C0731909     Student name - Inderjeet Singh
+    //Student id: C0726115       Student name - Robin bhatia
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Download complete");
+            Download();
+            Console.ReadLine();
+
+        }
+
+
+        static void Download()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Download  Complete");
         }
     }
 }
